34f85c8d8002c74acb582c0163e21b5c05826e67
sujoyghosal <sujoy.ghosal@gmail.com>
2017-11-13 10:58:34 +0530

    tested socket.io code

--- donation.js
+++ donation.js
@@ -20,12 +20,13 @@ var transporter = nodemailer.createTransport({
 //var config = require('./config');
 // Set up Express environment and enable it to read and write JavaScript
 var allowCrossDomain = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    //res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header(
         "Access-Control-Allow-Headers",
         "Content-Type, Authorization, Content-Length, X-Requested-With"
     );
+    res.setHeader("Access-Control-Allow-Headers", req.getHeader("Access-Control-Request-Headers"));
     // intercept OPTIONS method
     if ("OPTIONS" == req.method) {
         res.send(200);
