4081270a580bef9f1f4b6c04201731925c22d6a3
sujoyghosal <sujoy.ghosal@gmail.com>
2017-11-13 11:11:15 +0530

    Changed name in manifest

--- donation.js
+++ donation.js
@@ -21,7 +21,7 @@ var transporter = nodemailer.createTransport({
 // Set up Express environment and enable it to read and write JavaScript
 var allowCrossDomain = function(req, res, next) {
     //res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Origin", BASEGUIURL);
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header(
         "Access-Control-Allow-Headers",
