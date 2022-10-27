edd4f51dd7289545b0197e0e43385e3319f4da10
Sujoy Ghosal <sujoy.ghosal@gmail.com>
2022-10-27 09:58:07 +0530

    To IBM Cloud

--- donation.js
+++ donation.js
@@ -798,7 +798,18 @@ options = {
   },
   allowEIO3: true,
 });*/
-const io = require("socket.io")(httpServer, options);
+const io = require("socket.io")(httpServer, {
+  handlePreflightRequest: (req, res) => {
+    const headers = {
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Origin": "http://159.122.177.104:31055", //or the specific origin you want to give access to,
+      "Access-Control-Allow-Credentials": true,
+    };
+    res.writeHead(200, headers);
+    res.end();
+  },
+});
+//const io = require("socket.io")(httpServer, options);
 var mysocket = null;
 io.on("connection", function (socket) {
   mysocket = socket;
